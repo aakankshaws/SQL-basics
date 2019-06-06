83a17b0f465e48a7e88ff81d60505e1671ca818c
A.ws <aakankshaws@gmail.com>
2019-06-06 15:06:04 +0530

    DENSE_RANK example

---
+++ ClassesMoreThan5Students.sql
@@ -0,0 +1,35 @@
+/*There is a table courses with columns: student and class
+
+Please list out all classes which have more than or equal to 5 students.
+
+For example, the table:
+
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+Should output:
+
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+ 
+
+Note:
+The students should not be counted duplicate in each course. */
+
+SELECT DISTINCT sub.class
+FROM (SELECT *, DENSE_RANK() OVER(PARTITION BY class ORDER BY student ) rank
+     FROM courses ) sub
+WHERE sub.rank >=5;     
