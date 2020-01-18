d3d4e5c0d54db2da566211c17e568a2da68dabcd
Aakanksha W.S <aakankshaws@gmail.com>
2020-01-18 18:42:36 +0530

    Update JOIN Questions.sql

--- JOIN Questions.sql
+++ JOIN Questions.sql
@@ -20,3 +20,17 @@ ON r.id = s.region_id
 JOIN accounts a
 ON a.id = a.sales_rep_id
 ORDER BY a.name
+
+
+/*Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. Your final table should have 3 columns: region name, account name, and unit price. A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
+*/
+
+SELECT r.name region, a.name account, 
+       o.total_amt_usd/(o.total + 0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id;
